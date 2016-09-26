8a5203e0d9e994becac3898fb009017e64d0cd9d
meenasundarrajan <vinomeenasept15@gmail.com>
2016-09-26 14:42:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='Meena';
 
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.onclick=function(){
   img.style.marginleft='100px';  
 };
